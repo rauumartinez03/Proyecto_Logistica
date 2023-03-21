7864a662414f27a3f8272f2cdde00c344b8cfcbc
Luis Miguel Soria <lsoria@hamacapp.com>
2023-03-21 16:31:37 +0100

    Comment added
    Comment added

--- src/main/java/es/us/dad/mysql/rest/RestEntityMessage.java
+++ src/main/java/es/us/dad/mysql/rest/RestEntityMessage.java
@@ -1,5 +1,14 @@
 package es.us.dad.mysql.rest;
 
+/**
+ * An enum that represents the entity associated with the message that the Rest
+ * API exchanges with the controller. This entity differs from the one used
+ * between the controller and the database to avoid conflicts between the
+ * messages exchanged by both elements.
+ * 
+ * @author luismi
+ *
+ */
 public enum RestEntityMessage {
 	Sensor("SensorRest"), Actuator("ActuatorRest"), Group("GroupRest"), Device("DeviceRest"),
 	SensorValue("SensorValueRest"), ActuatorStatus("ActuatorStatusRest");
